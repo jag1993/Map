ef9c9c91c892498566574f42876bfddad7960735
Jose Alvaro Garcia <jag040393@gmail.com>
2016-11-20 17:03:59 -0800

    Reverted to old file

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -7,25 +9,22 @@ var methodOverride = require('method-override');
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({
-    extended: false
+	extended: false
 }));
 
 app.use(methodOverride('_method'));
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
+	defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
 
 
 var port = process.env.PORT || 7015;
-var server = app.listen(port,function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
-var io = require('socket.io').listen(server);
+var server = app.listen(port);
+var io      = require('socket.io').listen(server);
 
 var routes = require('./controllers/map_controller.js');
 
-routes(app, io);
+routes(app,io);
